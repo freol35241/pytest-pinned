2c6ef871ad290214333421a234ce36a24733da01
freol35241 <freol@outlook.com>
2020-05-22 18:03:00 +0200

    Releasing to PyPi

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pytest-pinned',
-    version='0.1.0',
+    version='0.1.1',
     author='Fredrik Olsson',
     author_email='freol@outlook.com',
     maintainer='Fredrik Olsson',
