14005ce1da708c07d8b90a6a871ec6148d4556ac
freol35241 <freol@outlook.com>
2020-06-03 21:59:52 +0200

    Bumping version to 0.3.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pytest-pinned',
-    version='0.2.1',
+    version='0.3.0',
     author='Fredrik Olsson',
     author_email='freol@outlook.com',
     maintainer='Fredrik Olsson',
